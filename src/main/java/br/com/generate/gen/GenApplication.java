8714b7d59e9c0ef5dca09d71183cb7e30eec086e
Wesley Rodrigues Soares <podwesley@gmail.com>
2020-02-07 01:52:44 -0300

    ::feat: configurações do springboot.

---
+++ src/main/java/br/com/generate/gen/GenApplication.java
@@ -0,0 +1,13 @@
+package br.com.generate.gen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenApplication.class, args);
+	}
+
+}
